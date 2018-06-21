10892c0dddf5f7a4ee303af70cd950ef5525528e
jcgenthe <jongenthe@gmail.com>
2018-06-21 18:40:47 -0500

    Finished

--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 public class JobForm {
 
     @NotNull
-    @Size(min=1, message = "Name may not be empty")
+    @Size(min=1, message = "Name May Not Be Empty")
     private String name;
 
     @NotNull
